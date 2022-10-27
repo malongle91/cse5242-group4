fd77c2b8a6d4d086555f5c26e62a211fcc1a169b
aaron-roy <aaroy321@gmail.com>
2022-10-26 23:26:51 -0400

    changes to the file

--- classroom-tracking/src/App.js
+++ classroom-tracking/src/App.js
@@ -2,10 +2,36 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+
+// const Stomp = require("stomp-client");
+
+// const stompClient = new Stomp("127.0.0.1", 61613);
+
+// stompClient.connect(function () {
+//     console.log("Producer connected.");
+
+//     const notification = {
+//         label: "You have a new notification!",
+//         name: "Bob Dylan",
+//     };
+
+//     const notificationJSON = JSON.stringify(notification);
+
+//     stompClient.publish("/queue/notifications", notificationJSON);
+
+//     stompClient.disconnect();
+// });
   // Properties
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [startQuiz, setStartQuiz] = useState(true)
+  const [userProperties, setUserProperties] = useState({
+    fname: '',
+    lname: '',
+    studentID: '',
+    seatGroup: ''
+  });
 
   const questions = [
     {
@@ -57,6 +83,14 @@ function App() {
 
   // Helper Functions
 
+  
+  const handleChange = event => {
+    const value = event.target.value;
+    setUserProperties({
+      ...userProperties,
+      [event.target.name]:value,
+    })
+  }
   /* A possible answer was clicked */
   const optionClicked = (isCorrect) => {
     // Increment the score
@@ -80,47 +114,71 @@ function App() {
 
   return (
     <div className="App">
-      {/* 1. Header  */}
+
+      { startQuiz ? ( 
+        <div className="form">
+          <form>
+            <br/>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" onChange={handleChange}/>
+            <label for="lname">Last name:</label>            
+            <input type="text" id="lname" name="lname" onChange={handleChange}/>
+            <label for="studentID">Student id:</label>
+            <input type="text" id="studentID" name="studentID" onChange={handleChange}/>
+            <label for="seatGroup">Seat group:</label>
+            <input type="text" id="seatGroup" name="seatGroup" onChange={handleChange}/>
+            <br/>
+            <button onClick={()=>{setStartQuiz(false)}}>Submit</button>
+          </form>
+          {console.log(userProperties.fname)}
+        </div>
+        
+      ):(
+        <div>
+        {/* 1. Header  */}
       <h1>USA Quiz 🇺🇸</h1>
 
-      {/* 2. Current Score  */}
-      <h2>Score: {score}</h2>
+{/* 2. Current Score  */}
+<h2>Score: {score}</h2>
 
-      {/* 3. Show results or show the question game  */}
-      {showResults ? (
-        /* 4. Final Results */
-        <div className="final-results">
-          <h1>Final Results</h1>
-          <h2>
-            {score} out of {questions.length} correct - (
-            {(score / questions.length) * 100}%)
-          </h2>
-          <button onClick={() => restartGame()}>Restart game</button>
-        </div>
-      ) : (
-        /* 5. Question Card  */
-        <div className="question-card">
-          {/* Current Question  */}
-          <h2>
-            Question: {currentQuestion + 1} out of {questions.length}
-          </h2>
-          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+{/* 3. Show results or show the question game  */}
+{showResults ? (
+  /* 4. Final Results */
+  <div className="final-results">
+    <h1>Final Results</h1>
+    <h2>
+      {score} out of {questions.length} correct - (
+      {(score / questions.length) * 100}%)
+    </h2>
+    <button onClick={() => restartGame()}>Restart game</button>
+  </div>
+) : (
+  /* 5. Question Card  */
+  <div className="question-card">
+    {/* Current Question  */}
+    <h2>
+      Question: {currentQuestion + 1} out of {questions.length}
+    </h2>
+    <h3 className="question-text">{questions[currentQuestion].text}</h3>
 
-          {/* List of possible answers  */}
-          <ul>
-            {questions[currentQuestion].options.map((option) => {
-              return (
-                <li
-                  key={option.id}
-                  onClick={() => optionClicked(option.isCorrect)}
-                >
-                  {option.text}
-                </li>
-              );
-            })}
-          </ul>
+    {/* List of possible answers  */}
+    <ul>
+      {questions[currentQuestion].options.map((option) => {
+        return (
+          <li
+            key={option.id}
+            onClick={() => optionClicked(option.isCorrect)}
+          >
+            {option.text}
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+)}  
         </div>
       )}
+      
     </div>
   );
 }

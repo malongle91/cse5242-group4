a17255b7ba382398bde9417b30d91a51ab13ce38
malongle91 <63027244+malongle91@users.noreply.github.com>
2022-11-04 20:25:46 -0400

    Got the triggers working for answer counts and hotspot detection. + Created a trigger upon student answer insertion to update the total number of answers and correct answers for that student. + Created a trigger before student answer insertion to detect any hotspots, looking at the other student ansewers within the same student group.

---
+++ db/triggers/hotspot_update.sql
@@ -0,0 +1,37 @@
+/*
+ * Trigger to create a hotspot upon insertion of a new student answer
+ * if the trigger finds a student of the same group who has gotten
+ * the same wrong answer on the same question.
+ */
+CREATE TRIGGER hotspot_update
+    BEFORE INSERT
+    ON Student_Answer
+    -- Condition 1: Student's answer to given question is wrong.
+    -- Condition 2: There exists another student answer in the same group with the same wrong answer.
+    WHEN (new.student_answer != (SELECT correct_answer
+                                 FROM Question q
+                                 WHERE q.question_id = new.question_id))
+        AND EXISTS(
+                 SELECT *
+                 FROM (SELECT seat_group_no AS no FROM Student WHERE student_id = new.student_id) AS target_group,
+                      (Student_Answer sa NATURAL JOIN Student s)
+                 WHERE sa.question_id = new.question_id
+                   AND sa.student_answer = new.student_answer
+                   AND s.seat_group_no = target_group.no
+             )
+BEGIN
+    -- Insert into Hotspot the names of the students in the same group who have the
+    -- same wrong answer to the given question.
+    INSERT OR IGNORE INTO Hotspot (seat_group_no, question_id, student_id, incorrect_answer)
+    SELECT no, new.question_id, student_id, new.student_answer
+    FROM (SELECT seat_group_no AS no FROM Student WHERE student_id = new.student_id) AS target_group,
+         (Student s NATURAL JOIN Student_Answer sa)
+    WHERE sa.question_id = new.question_id
+      AND sa.student_answer = new.student_answer
+      AND s.seat_group_no = target_group.no;
+
+    -- Include the student who triggered the hotspot.
+    INSERT INTO Hotspot (seat_group_no, question_id, student_id, incorrect_answer)
+    VALUES ((SELECT seat_group_no AS no FROM Student WHERE student_id = new.student_id), new.question_id,
+            new.student_id, new.student_answer);
+end;
---
+++ db/triggers/student_num_correct_update.sql
@@ -0,0 +1,16 @@
+/*
+ * Trigger to update the student's correct and total answered count
+ * upon answering a new question.
+ */
+CREATE TRIGGER student_num_correct_update
+    AFTER INSERT ON Student_Answer
+BEGIN
+    UPDATE Student
+    SET num_answered = num_answered + 1,
+        num_correct =
+        num_correct + IIF(
+            new.student_answer =
+                (SELECT correct_answer FROM Question q WHERE q.question_id = new.question_id),
+            1, 0)
+    WHERE Student.student_id = new.student_id;
+end;

4dcb0e89e0fa0214e6157c35fffd7168a956b354
malongle91 <63027244+malongle91@users.noreply.github.com>
2022-11-06 00:09:02 -0400

    Added a working Stomp connection from the Quiz to the ActiveMQ broker. + Added 'stompjs' to the quiz dependencies. + Tweaked the quiz form to funnel form data into the right function calls. + Added functions that send student and student answer info to the '/queue/quiz_results' topic in ActiveMQ.

--- classroom-tracking/src/App.js
+++ classroom-tracking/src/App.js
@@ -1,42 +1,24 @@
 import React, { useState } from "react";
 import "./App.css";
-import SockJsClient from "react-stomp";
+const Stomp = require('stompjs');
 
+const STOMP_URL = 'ws://127.0.0.1:61614';
+const TOPIC_PATH = '/queue/quiz_results';
+
+const LOCAL_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+};
+
+const client = Stomp.client(STOMP_URL);
+client.connect(LOCAL_HEADERS, () => { console.log("Connected to ActiveMQ.") });
 
 function App() {
-
-  
   // Properties
-  
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const [startQuiz, setStartQuiz] = useState(true)
-  const [userProperties, setUserProperties] = useState({
-    fname: '',
-    lname: '',
-    studentID: '',
-    seatGroup: ''
-  });
-
-  const [ clientRef, setClientRef] = useState({})
-
-  const sendMessage = () => {
-    try {
-      console.log(clientRef)
-      clientRef.sendMessage("/somethingRandom", JSON.stringify("selfMsg"));
-      return true;
-    } catch(e) {
-      console.log("not connected")
-      return false;
-    }
-  }
-
-  const onMessageReceive = (msg, topic) => {
-    this.setState(prevState => ({
-      messages: [...prevState.messages, msg]
-    }));
-  }
+  const [startQuiz, setStartQuiz] = useState(true);
+  const [studentId, setStudentId] = useState('');
 
   const questions = [
     {
@@ -88,14 +70,6 @@ function App() {
 
   // Helper Functions
 
-  
-  const handleChange = event => {
-    const value = event.target.value;
-    setUserProperties({
-      ...userProperties,
-      [event.target.name]:value,
-    })
-  }
   /* A possible answer was clicked */
   const optionClicked = (isCorrect) => {
     // Increment the score
@@ -114,82 +88,103 @@ function App() {
   const restartGame = () => {
     setScore(0);
     setCurrentQuestion(0);
+    setStartQuiz(true);
     setShowResults(false);
   };
 
+  const sendStudent = (msg) => {
+    client.send(TOPIC_PATH, LOCAL_HEADERS, JSON.stringify(msg));
+  }
+
+  const sendStudentAnswer = (msg) => {
+    var message = {
+      "question_id": (currentQuestion + 1),
+      "student_id": studentId,
+      "student_answer": String.fromCharCode(65 + msg.id) // Ranges from 'A' to 'D'.
+    };
+    client.send(TOPIC_PATH, LOCAL_HEADERS, JSON.stringify(message));
+  }
+
   return (
     <div className="App">
-      <SockJsClient url={ "http://localhost:61613" } topics={["/topics/somethingRandom"]} 
-          onMessage={ onMessageReceive } ref={ (client) => { setClientRef(client) }}
-          onConnect={ () => { this.setState({ clientConnected: true }) } }
-          onDisconnect={ () => { this.setState({ clientConnected: false }) } }
-          debug={ false }/>
-
-      { startQuiz ? ( 
+      {startQuiz ? (
         <div className="form">
-          <form>
-            <br/>
-            <label for="fname">First name:</label>
-            <input type="text" id="fname" name="fname" onChange={handleChange}/>
-            <label for="lname">Last name:</label>            
-            <input type="text" id="lname" name="lname" onChange={handleChange}/>
-            <label for="studentID">Student id:</label>
-            <input type="text" id="studentID" name="studentID" onChange={handleChange}/>
-            <label for="seatGroup">Seat group:</label>
-            <input type="text" id="seatGroup" name="seatGroup" onChange={handleChange}/>
-            <br/>
-            <button onClick={()=>{setStartQuiz(false); sendMessage(); console.log("message sent");}}>Submit</button>
+          <form onSubmit={() => {
+            setStudentId(document.getElementById("studentID").value);
+            setStartQuiz(false);
+            sendStudent({
+              "student_id": document.getElementById("studentID").value,
+              "first_name": document.getElementById("fname").value,
+              "last_name": document.getElementById("lname").value,
+              "seat_group_no": parseInt(document.getElementById("seatGroup").value),
+            });
+          }}>
+            <br />
+            <label htmlFor="fname">First Name: </label>
+            <input type="text" id="fname" name="fname" required/>
+            <br />
+            <label htmlFor="lname">Last Name: </label>
+            <input type="text" id="lname" name="lname" required/>
+            <br />
+            <label htmlFor="studentID">Student Id: </label>
+            <input type="text" id="studentID" name="studentID" required/>
+            <br />
+            <label htmlFor="seatGroup">Seat Group: </label>
+            <input type="number" id="seatGroup" name="seatGroup" required/>
+            <br />
+            <input type="submit" value="Submit"/>
           </form>
-          
-          {console.log(userProperties.fname)}
         </div>
-        
-      ):(
+
+      ) : (
         <div>
-        {/* 1. Header  */}
-      <h1>USA Quiz 🇺🇸</h1>
+          {/* 1. Header  */}
+          <h1>USA Quiz 🇺🇸</h1>
 
-{/* 2. Current Score  */}
-<h2>Score: {score}</h2>
+          {/* 2. Current Score  */}
+          <h2>Score: {score}</h2>
 
-{/* 3. Show results or show the question game  */}
-{showResults ? (
-  /* 4. Final Results */
-  <div className="final-results">
-    <h1>Final Results</h1>
-    <h2>
-      {score} out of {questions.length} correct - (
-      {(score / questions.length) * 100}%)
-    </h2>
-    <button onClick={() => restartGame()}>Restart game</button>
-  </div>
-) : (
-  /* 5. Question Card  */
-  <div className="question-card">
-    {/* Current Question  */}
-    <h2>
-      Question: {currentQuestion + 1} out of {questions.length}
-    </h2>
-    <h3 className="question-text">{questions[currentQuestion].text}</h3>
+          {/* 3. Show results or show the question game  */}
+          {showResults ? (
+            /* 4. Final Results */
+            <div className="final-results">
+              <h1>Final Results</h1>
+              <h2>
+                {score} out of {questions.length} correct - (
+                {(score / questions.length) * 100}%)
+              </h2>
+              <button onClick={() => restartGame()}>Restart game</button>
+            </div>
+          ) : (
+            /* 5. Question Card  */
+            <div className="question-card">
+              {/* Current Question  */}
+              <h2>
+                Question: {currentQuestion + 1} out of {questions.length}
+              </h2>
+              <h3 className="question-text">{questions[currentQuestion].text}</h3>
 
-    {/* List of possible answers  */}
-    <ul>
-      {questions[currentQuestion].options.map((option) => {
-        return (
-          <li
-            key={option.id}
-            onClick={() => optionClicked(option.isCorrect)}
-          >
-            {option.text}
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-)}  
+              {/* List of possible answers  */}
+              <ul>
+                {questions[currentQuestion].options.map((option) => {
+                  return (
+                    <li
+                      key={option.id}
+                      onClick={() => {
+                        optionClicked(option.isCorrect);
+                        sendStudentAnswer(option);
+                      }}
+                    >
+                      {option.text}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
         </div>
       )}
-      
+
     </div>
   );
 }
